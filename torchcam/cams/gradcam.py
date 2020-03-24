7166fc56788ba20532c0b420240afa39729ce0f6
F-G Fernandez <fgfm03@hotmail.fr>
2020-03-24 18:35:12 +0100

    fix: Fixed GradCAM on non-default device

--- torchcam/cams/gradcam.py
+++ torchcam/cams/gradcam.py
@@ -148,7 +148,7 @@ class SmoothGradCAMpp(_GradCAM):
         # Perform the operations N times
         for _idx in range(self.num_samples):
             # Add noise
-            noisy_input = self._input + self._distrib.sample(self._input.size())
+            noisy_input = self._input + self._distrib.sample(self._input.size()).to(device=self._input.device)
             # Forward & Backward
             out = self.model(noisy_input)
             self.model.zero_grad()
