299831fb4f80ea85aaaf34f0ba64920954c8e259
frgfm <fgfm03@hotmail.fr>
2020-12-29 08:47:06 +0000

    [skip ci] Documentation updates

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.3a0+3456ba9-git',
+    VERSION: '0.1.3a0+7a95e14-git',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
