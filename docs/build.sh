f8e4dd236ab69e1c0a9c3e83a2b261db049f05ef
F-G Fernandez <fgfm03@hotmail.fr>
2021-03-14 18:19:34 +0100

    docs: Pinned conf.py for all doc versions (#56)
    * docs: Pin conf.py for all doc versions
    
    * chore: Fixed conf pinning
    
    * docs: Fixed conf pinning
    
    * docs: Fixed conditional check

--- docs/build.sh
+++ docs/build.sh
@@ -11,27 +11,28 @@ function deploy_doc(){
         if [ "$2" == "latest" ]; then
             echo "Pushing master"
             sphinx-build source _build -a && mkdir build && mkdir build/$2 && cp -a _build/* build/$2/
-        elif ssh -oStrictHostKeyChecking=no $doc "[ -d build/$2 ]"; then
+        elif [ -d build/$2 ]; then
             echo "Directory" $2 "already exists"
         else
             echo "Pushing version" $2
-            cp -r _static source/
+            cp -r _static source/ && cp _conf.py source/conf.py
             sphinx-build source _build -a
-            mkdir build/$2 && cp -a _build/* build/$2/
+            mkdir build/$2 && cp -a _build/* build/$2/ && git checkout source/
         fi
     else
         echo "Pushing stable"
-        cp -r _static source/
-        sphinx-build source build -a
+        cp -r _static source/ && cp _conf.py source/conf.py
+        sphinx-build source build -a && git checkout source/
     fi
 }
 
 # You can find the commit for each tag on https://github.com/frgfm/torch-cam/tags
 if [ -d build ]; then rm -Rf build; fi
 cp -r source/_static .
+cp source/conf.py _conf.py
 git fetch --all --tags --unshallow
 deploy_doc "" latest
 deploy_doc "7be0b4f" v0.1.0
 deploy_doc "a95d680" v0.1.1
 deploy_doc "521b4f9" # v0.1.2 Latest stable release
-rm -rf _build _static
+rm -rf _build _static _conf.py
