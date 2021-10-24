9103902c9d0e1c6719c50ef57fc18ab195ddec8b
frgfm <fgfm03@hotmail.fr>
2021-10-24 20:21:57 +0200

    test: Added CAM fusion unittest

--- test/test_cams_core.py
+++ test/test_cams_core.py
@@ -96,3 +96,29 @@ def test_cam_repr(mock_img_model):
     extractor = core._CAM(model, '0.3')
 
     assert repr(extractor) == "_CAM(target_layer=['0.3'])"
+
+
+def test_fuse_cams():
+
+    with pytest.raises(TypeError):
+        core._CAM.fuse_cams(torch.zeros((3, 32, 32)))
+
+    with pytest.raises(ValueError):
+        core._CAM.fuse_cams([])
+
+    cams = [torch.rand((32, 32)), torch.rand((16, 16))]
+
+    # Single CAM
+    assert torch.equal(cams[0], core._CAM(cams[:1]))
+
+    # Fusion
+    cam = core._CAM.fuse_cams(cams)
+    assert isinstance(cam, torch.Tensor)
+    assert cam.ndim = cams[0].ndim
+    assert cam.shape == (32, 32)
+
+    # Specify target shape
+    cam = core._CAM.fuse_cams(cams, (16, 16))
+    assert isinstance(cam, torch.Tensor)
+    assert cam.ndim = cams[0].ndim
+    assert cam.shape == (16, 16)
