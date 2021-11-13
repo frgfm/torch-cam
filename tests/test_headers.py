bfe18f3ca19dd3553a12b775753b4fbfc35dccc1
frgfm <fgfm03@hotmail.fr>
2021-11-13 01:48:36 +0100

    test: Added unittest to validate headers

---
+++ tests/test_headers.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from pathlib import Path
+
+
+def test_headers():
+
+    shebang = ["#!usr/bin/python\n"]
+    blank_line = "\n"
+
+    copyright_notice = [f"# Copyright (C) 2020-{datetime.now().year}, François-Guillaume Fernandez.\n"]
+    license_notice = [
+        "# This program is licensed under the Apache License version 2.\n",
+        "# See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.\n"
+    ]
+
+    # Define all header options
+    headers = [
+        shebang + [blank_line] + copyright_notice + [blank_line] + license_notice,
+        copyright_notice + [blank_line] + license_notice
+    ]
+
+    excluded_files = ["version.py", "__init__.py"]
+    invalid_files = []
+
+    # For every python file in the repository
+    for source_path in Path(__file__).parent.parent.joinpath('torchcam').rglob('**/*.py'):
+        if source_path.name not in excluded_files:
+            # Parse header
+            header_length = max(len(option) for option in headers)
+            current_header = []
+            with open(source_path) as f:
+                for idx, line in enumerate(f):
+                    current_header.append(line)
+                    if idx == header_length - 1:
+                        break
+            # Validate it
+            if not any(
+                "".join(current_header[:min(len(option), len(current_header))]) == "".join(option)
+                for option in headers
+            ):
+                invalid_files.append(source_path)
+
+    assert len(invalid_files) == 0, f"Invalid header in the following files: {invalid_files}"
